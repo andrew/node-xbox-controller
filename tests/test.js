1f793cad1ce6d0c8339c0ea4624ecc9b9de41dc4
Rick Waldron <waldron.rick@gmail.com>
2013-12-31 10:34:51 -0500

    Run through jsbeautifier
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- tests/test.js
+++ tests/test.js
@@ -7,22 +7,22 @@ xbox.on('a:press', function (key) {
 });
 
 xbox.on('b:release', function (key) {
-    console.log('b release');
+  console.log('b release');
 });
 
 
-xbox.on('lefttrigger', function(position){
+xbox.on('lefttrigger', function (position) {
   console.log('lefttrigger', position)
-})
+});
 
-xbox.on('righttrigger', function(position){
+xbox.on('righttrigger', function (position) {
   console.log('righttrigger', position)
-})
+});
 
-xbox.on('left:move', function(position){
+xbox.on('left:move', function (position) {
   console.log('left:move', position)
-})
+});
 
-xbox.on('right:move', function(position){
+xbox.on('right:move', function (position) {
   console.log('right:move', position)
-})
+});
